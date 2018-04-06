1525151f303047e9f44ede2137926f46c0b93738
Lou Ahola <lou@hashchain.ca>
2018-04-06 09:47:08 -0400

    updating license to GPLv3

--- smsh/command/__init__.py
+++ smsh/command/__init__.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2018 Lou Ahola, HashChain Technology, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import re
 
 from smsh.command.buffered import BufferedCommand
@@ -7,6 +22,7 @@ from smsh.command.unbuffered import UnbufferedCommand
 
 CMD_EXIT_PATTERN = re.compile("^(exit|quit)$")
 CMD_EDITOR_PATTERN = re.compile("^(vi|vim|nano) \S+")
+LICENSE_PATTERN = re.compile("^smsh\-license")
 
 
 def create(*, command, buffered_output):
